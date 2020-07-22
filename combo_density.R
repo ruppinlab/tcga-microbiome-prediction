74eea7f6053e298e10152af42206a0032b8201be
E. Michael Gertz <gertzem@nih.gov>
2020-07-21 20:51:58 -0400

    Remove some dead code from combo_density.R

--- combo_density.R
+++ combo_density.R
@@ -1,31 +1,24 @@
 library(readr)
 library(ggplot2)
 library(dplyr)
-library(RColorBrewer)
 
-outdir <- 'figures/combo_density_plots'
-dir.create(outdir, recursive=TRUE, showWarnings=FALSE)
-cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+outdir <- "figures/combo_density_plots"
+dir.create(outdir, recursive = TRUE, showWarnings = FALSE)
+cbPalette <- c(
+  "#999999", "#E69F00", "#56B4E9", "#009E73",
+  "#F0E442", "#0072B2", "#D55E00", "#CC79A7"
+)
 
-compare_runs <- read_tsv("analysis/goodness_hits.txt", col_types = cols())
-compare_runs <- compare_runs %>%
-  filter(
-      analysis != "rest" &
-      features == "combo" &
-      avg_test >= .6
-  )
-compare_runs
-
-runs = tibble(
-  cancer = c('BLCA', 'CESC', 'HNSC', 'SARC' ),
-  versus = c('Cisplatin', 'Cisplatin', 'Carboplatin', 'Docetaxel')
+runs <- tibble(
+  cancer = c("BLCA", "CESC", "HNSC", "SARC"),
+  versus = c("Cisplatin", "Cisplatin", "Carboplatin", "Docetaxel")
 )
 
 tests <- read_tsv("analysis/model_goodness.txt", col_types = cols())
-micro = tests %>% filter(features == 'kraken' & analysis == 'resp')
-expr = tests %>% filter(features == 'htseq' & analysis == 'resp')
-combo = tests %>% filter(features == 'combo' & analysis == 'resp')
-micro_expr = inner_join(
+micro <- tests %>% filter(features == "kraken" & analysis == "resp")
+expr <- tests %>% filter(features == "htseq" & analysis == "resp")
+combo <- tests %>% filter(features == "combo" & analysis == "resp")
+micro_expr <- inner_join(
   micro,
   expr,
   by = c("cancer", "analysis", "versus", "how", "index")
@@ -33,61 +26,67 @@ micro_expr = inner_join(
   rename(micro_goodness = goodness.x, expr_goodness = goodness.y) %>%
   filter(is.finite(micro_goodness) & is.finite(expr_goodness)) %>%
   select(-c(how, features.x, features.y))
-joined_goodness = inner_join(
+joined_goodness <- inner_join(
   micro_expr,
   combo,
-  by = c("cancer", "analysis", "versus",  "index")
-) %>% rename(combo_goodness = goodness) %>%
+  by = c("cancer", "analysis", "versus", "index")
+) %>%
+  rename(combo_goodness = goodness) %>%
   select(-c(how, features))
 joined_goodness
 
-plots = list()
+plots <- list()
 for (i in seq(nrow(runs))) {
-dat <- joined_goodness %>%
-  filter(
-    cancer == runs$cancer[i] &
-    versus == runs$versus[i]
-  )
+  dat <- joined_goodness %>%
+    filter(
+      cancer == runs$cancer[i] &
+        versus == runs$versus[i]
+    )
 
-dat_plot <- data.frame(
-  ROC = c(dat$micro_goodness, dat$expr_goodness, dat$combo_goodness),
-  Features = c(
-    rep("Microbiome", length(dat$micro_goodness)),
-    rep("Expression", length(dat$expr_goodness)),
-    rep("Combination", length(dat$combo_goodness))
+  dat_plot <- data.frame(
+    ROC = c(dat$micro_goodness, dat$expr_goodness, dat$combo_goodness),
+    Features = c(
+      rep("Microbiome", length(dat$micro_goodness)),
+      rep("Expression", length(dat$expr_goodness)),
+      rep("Combination", length(dat$combo_goodness))
+    )
   )
-)
-dat_plot$Features = factor(dat_plot$Features, levels = c("Microbiome", "Expression", "Combination"))
-plots[[i]] <- 
-ggplot(dat_plot, aes(x = ROC, fill = Features)) + 
+  dat_plot$Features <- factor(
+    dat_plot$Features,
+    levels = c("Microbiome", "Expression", "Combination")
+  )
+  plots[[i]] <-
+    ggplot(dat_plot, aes(x = ROC, fill = Features)) +
     theme_minimal() +
-  scale_fill_manual(values = c(cbPalette[3], cbPalette[7], cbPalette[8])) +
-  geom_density(alpha = .2) +
+    scale_fill_manual(values = c(cbPalette[3], cbPalette[7], cbPalette[8])) +
+    geom_density(alpha = .2) +
     xlim(0, 1) +
-    xlab('AUROC') +
-    ylab('Density') +
-    theme(    legend.position = 'none',
-              plot.title = element_text(size = 20),
-    axis.text.x = element_text(size = 16),
-    axis.text.y = element_text(size = 16),
-    legend.title = element_text(size = 16),
-    legend.text = element_text(size = 16),
-    axis.title.x = element_text(size = 20),
-    axis.title.y = element_text(size = 20)) +
+    xlab("AUROC") +
+    ylab("Density") +
+    theme(
+      legend.position = "none",
+      plot.title = element_text(size = 20),
+      axis.text.x = element_text(size = 16),
+      axis.text.y = element_text(size = 16),
+      legend.title = element_text(size = 16),
+      legend.text = element_text(size = 16),
+      axis.title.x = element_text(size = 20),
+      axis.title.y = element_text(size = 20)
+    ) +
     ggtitle(paste(runs$cancer[i], runs$versus[[i]]))
-print(plots[[i]])
+  print(plots[[i]])
 }
 
-runs = tibble(
-  cancer = c('THYM'),
-  versus = c('OS')
+runs <- tibble(
+  cancer = c("THYM"),
+  versus = c("OS")
 )
 
 tests <- read_tsv("analysis/model_goodness.txt", col_types = cols())
-micro = tests %>% filter(features == 'kraken' & analysis == 'surv')
-expr = tests %>% filter(features == 'htseq' & analysis == 'surv')
-combo = tests %>% filter(features == 'combo' & analysis == 'surv')
-micro_expr = inner_join(
+micro <- tests %>% filter(features == "kraken" & analysis == "surv")
+expr <- tests %>% filter(features == "htseq" & analysis == "surv")
+combo <- tests %>% filter(features == "combo" & analysis == "surv")
+micro_expr <- inner_join(
   micro,
   expr,
   by = c("cancer", "analysis", "versus", "how", "index")
@@ -95,60 +94,64 @@ micro_expr = inner_join(
   rename(micro_goodness = goodness.x, expr_goodness = goodness.y) %>%
   filter(is.finite(micro_goodness) & is.finite(expr_goodness)) %>%
   select(-c(how, features.x, features.y))
-joined_goodness = inner_join(
+joined_goodness <- inner_join(
   micro_expr,
   combo,
-  by = c("cancer", "analysis", "versus",  "index")
-) %>% rename(combo_goodness = goodness) %>%
+  by = c("cancer", "analysis", "versus", "index")
+) %>%
+  rename(combo_goodness = goodness) %>%
   select(-c(how, features))
 joined_goodness
 
-surv_plots = list()
+surv_plots <- list()
 for (i in seq(nrow(runs))) {
-dat <- joined_goodness %>%
-  filter(
-    cancer == runs$cancer[i] &
-    versus == runs$versus[i]
-  )
+  dat <- joined_goodness %>%
+    filter(
+      cancer == runs$cancer[i] &
+        versus == runs$versus[i]
+    )
 
-dat_plot <- data.frame(
-  ROC = c(dat$micro_goodness, dat$expr_goodness, dat$combo_goodness),
-  Features = c(
-    rep("Microbiome", length(dat$micro_goodness)),
-    rep("Expression", length(dat$expr_goodness)),
-    rep("Combination", length(dat$combo_goodness))
+  dat_plot <- data.frame(
+    ROC = c(dat$micro_goodness, dat$expr_goodness, dat$combo_goodness),
+    Features = c(
+      rep("Microbiome", length(dat$micro_goodness)),
+      rep("Expression", length(dat$expr_goodness)),
+      rep("Combination", length(dat$combo_goodness))
+    )
   )
-)
-dat_plot$Features = factor(dat_plot$Features, levels = c("Microbiome", "Expression", "Combination"))
-surv_plots[[i]] <- 
-ggplot(dat_plot, aes(x = ROC, fill = Features)) + 
+  dat_plot$Features <- factor(dat_plot$Features,
+    levels = c("Microbiome", "Expression", "Combination")
+  )
+  surv_plots[[i]] <-
+    ggplot(dat_plot, aes(x = ROC, fill = Features)) +
     theme_minimal() +
-  scale_fill_manual(values = c(cbPalette[3], cbPalette[7], cbPalette[8])) +
-  geom_density(alpha = .2) +
+    scale_fill_manual(values = c(cbPalette[3], cbPalette[7], cbPalette[8])) +
+    geom_density(alpha = .2) +
     xlim(0, 1) +
-    xlab('AUROC') +
-    ylab('Density') +
-    theme(    legend.position = 'none',
-              plot.title = element_text(size = 20),
-    axis.text.x = element_text(size = 16),
-    axis.text.y = element_text(size = 16),
-    legend.title = element_text(size = 16),
-    legend.text = element_text(size = 16),
-    axis.title.x = element_text(size = 20),
-    axis.title.y = element_text(size = 20)) +
+    xlab("AUROC") +
+    ylab("Density") +
+    theme(
+      legend.position = "none",
+      plot.title = element_text(size = 20),
+      axis.text.x = element_text(size = 16),
+      axis.text.y = element_text(size = 16),
+      legend.title = element_text(size = 16),
+      legend.text = element_text(size = 16),
+      axis.title.x = element_text(size = 20),
+      axis.title.y = element_text(size = 20)
+    ) +
     ggtitle(paste(runs$cancer[i], runs$versus[[i]]))
-print(surv_plots[[i]])
+  print(surv_plots[[i]])
 }
 
 for (i in seq_along(plots)) {
-  pdf(file.path(outdir, paste0('drug_response_combo_density', i, '.pdf')))
+  pdf(file.path(outdir, paste0("drug_response_combo_density", i, ".pdf")))
   print(plots[[i]])
   dev.off()
 }
 
 for (i in seq_along(surv_plots)) {
-  pdf(file.path(outdir, paste0('survival_combo_density', i, '.pdf')))
+  pdf(file.path(outdir, paste0("survival_combo_density", i, ".pdf")))
   print(surv_plots[[i]])
   dev.off()
 }
-
