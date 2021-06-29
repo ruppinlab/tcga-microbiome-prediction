a364bd436e4e2c24ca55980c14fa6732509cd2f4
Leandro Hermida <hermidal@cs.umd.edu>
2021-06-29 17:32:56 -0400

    Revert formatting

--- sklearn_extensions/feature_selection/_rna_seq.R
+++ sklearn_extensions/feature_selection/_rna_seq.R
@@ -4,25 +4,27 @@
 suppressPackageStartupMessages(library("edgeR"))
 
 
-edger_filterbyexpr_mask <- function(X, y, sample_meta = NULL, model_batch = FALSE, is_classif = TRUE) {
-  dge <- DGEList(counts = t(X))
-  if (
-    model_batch && !is.null(sample_meta) &&
-      length(unique(sample_meta$Batch)) > 1
-  ) {
-    sample_meta$Batch <- factor(sample_meta$Batch)
-    if (is_classif) {
-      sample_meta$Class <- factor(sample_meta$Class)
-      design <- model.matrix(~ Batch + Class, data = sample_meta)
+edger_filterbyexpr_mask <- function(
+    X, y, sample_meta=NULL, model_batch=FALSE, is_classif=TRUE
+) {
+    dge <- DGEList(counts=t(X))
+    if (
+        model_batch && !is.null(sample_meta) &&
+        length(unique(sample_meta$Batch)) > 1
+    ) {
+        sample_meta$Batch <- factor(sample_meta$Batch)
+        if (is_classif) {
+            sample_meta$Class <- factor(sample_meta$Class)
+            design <- model.matrix(~Batch + Class, data=sample_meta)
+        } else {
+            design <- model.matrix(~Batch, data=sample_meta)
+        }
+    } else if (is_classif) {
+        design <- model.matrix(~factor(y))
     } else {
-      design <- model.matrix(~Batch, data = sample_meta)
+        design <- NULL
     }
-  } else if (is_classif) {
-    design <- model.matrix(~ factor(y))
-  } else {
-    design <- NULL
-  }
-  return(filterByExpr(dge, design))
+    return(filterByExpr(dge, design))
 }
 
 # adapted from edgeR::calcNormFactors source code
--- sklearn_extensions/preprocessing/_rna_seq.R
+++ sklearn_extensions/preprocessing/_rna_seq.R
@@ -5,28 +5,28 @@ suppressPackageStartupMessages(library("edgeR"))
 
 
 # adapted from edgeR::calcNormFactors source code
-edger_tmm_ref_column <- function(counts, lib.size = colSums(counts), p = 0.75) {
-  y <- t(t(counts) / lib.size)
-  f <- apply(y, 2, function(x) quantile(x, p = p))
-  ref_column <- which.min(abs(f - mean(f)))
+edger_tmm_ref_column <- function(counts, lib.size=colSums(counts), p=0.75) {
+    y <- t(t(counts) / lib.size)
+    f <- apply(y, 2, function(x) quantile(x, p=p))
+    ref_column <- which.min(abs(f - mean(f)))
 }
 
-edger_tmm_logcpm_fit <- function(X, prior_count = 1) {
-  counts <- t(X)
-  dge <- DGEList(counts = counts)
-  dge <- calcNormFactors(dge, method = "TMM")
-  log_cpm <- cpm(dge, log = TRUE, prior.count = prior_count)
-  ref_sample <- counts[, edger_tmm_ref_column(counts)]
-  return(list(t(log_cpm), ref_sample))
+edger_tmm_logcpm_fit <- function(X, prior_count=1) {
+    counts <- t(X)
+    dge <- DGEList(counts=counts)
+    dge <- calcNormFactors(dge, method="TMM")
+    log_cpm <- cpm(dge, log=TRUE, prior.count=prior_count)
+    ref_sample <- counts[, edger_tmm_ref_column(counts)]
+    return(list(t(log_cpm), ref_sample))
 }
 
-edger_tmm_logcpm_transform <- function(X, ref_sample, prior_count = 1) {
-  counts <- t(X)
-  counts <- cbind(counts, ref_sample)
-  colnames(counts) <- NULL
-  dge <- DGEList(counts = counts)
-  dge <- calcNormFactors(dge, method = "TMM", refColumn = ncol(dge))
-  log_cpm <- cpm(dge, log = TRUE, prior.count = prior_count)
-  log_cpm <- log_cpm[, -ncol(log_cpm)]
-  return(t(log_cpm))
+edger_tmm_logcpm_transform <- function(X, ref_sample, prior_count=1) {
+    counts <- t(X)
+    counts <- cbind(counts, ref_sample)
+    colnames(counts) <- NULL
+    dge <- DGEList(counts=counts)
+    dge <- calcNormFactors(dge, method="TMM", refColumn=ncol(dge))
+    log_cpm <- cpm(dge, log=TRUE, prior.count=prior_count)
+    log_cpm <- log_cpm[, -ncol(log_cpm)]
+    return(t(log_cpm))
 }
