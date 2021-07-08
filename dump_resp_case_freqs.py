aac52553ea35a334886bffdea12cb935233cf61b
Leandro Hermida <hermidal@cs.umd.edu>
2021-07-08 14:03:08 -0400

    Add drug resp case info tsv and updated col headers

--- dump_resp_case_freqs.py
+++ dump_resp_case_freqs.py
@@ -24,12 +24,16 @@ pandas2ri.activate()
 
 parser = ArgumentParser()
 parser.add_argument('--data-dir', type=str, default='data', help='data dir')
+parser.add_argument('--results-dir', type=str, default='results/models',
+                    help='results dir')
 parser.add_argument('--sort-by', type=str, nargs='+',
                     choices=['Dataset Name', 'Analysis', 'Num Cases',
-                             '- Cases', '+ Cases'],
+                             'NR (-) Cases', 'R (+) Cases'],
                     default=['Analysis', 'Dataset Name'],
                     help='columns to sort table by')
 args = parser.parse_args()
+
+analysis = 'resp'
 
 r_base = importr('base')
 r_biobase = importr('Biobase')
@@ -48,6 +52,10 @@ for eset_file in sorted(eset_files):
     results.append([dataset_name, analysis, num_cases, neg_cases, pos_cases])
 
 results_df = pd.DataFrame(results, columns=[
-    'Dataset Name', 'Analysis', 'Num Cases', '- Cases', '+ Cases'])
+    'Dataset Name', 'Analysis', 'Num Cases', 'NR (-) Cases', 'R (+) Cases'])
+out_dir = '{}/{}'.format(args.results_dir, analysis)
+os.makedirs(out_dir, mode=0o755, exist_ok=True)
+results_df.to_csv('{}/resp_case_freqs.tsv'.format(out_dir), sep='\t',
+                  index=False)
 print(tabulate(results_df.sort_values(by=args.sort_by), headers='keys',
                showindex=False))
