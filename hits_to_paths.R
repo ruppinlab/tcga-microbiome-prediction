af866d0545247b97ee5c312e717caca391deffac
E. Michael Gertz <gertzem@nih.gov>
2021-07-20 11:01:53 -0400

    Create hits_to_paths.R, generalizing kraken_hits_to_paths

---
+++ hits_to_paths.R
@@ -0,0 +1,30 @@
+suppressMessages({
+  library(dplyr)
+  library(readr)
+})
+
+args <- commandArgs(trailingOnly = TRUE)
+features <- args[1]
+hits <- read_tsv(args[2], col_types = cols())
+hits <- hits %>%
+  filter(features == !!features &
+    ((analysis == "resp" & how == "RFE") |
+      (analysis == "surv" & how == "CNET"))) %>%
+  mutate(features = ifelse(
+    features == "kraken", features, paste0(features, "_counts")
+  ))
+
+cancer <- tolower(hits$cancer)
+versus <- tolower(hits$versus)
+how <- tolower(hits$how)
+name <- paste(
+  "tcga", cancer, hits$analysis, versus, hits$features, how,
+  sep = "_"
+)
+path <- file.path(
+  "results/models",
+  hits$analysis,
+  name,
+  paste(name, "feature_weights.rds", sep = "_")
+)
+cat(path, sep = "\n")
