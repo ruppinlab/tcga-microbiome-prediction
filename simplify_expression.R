6b987c3fa49538aed833e61b37db9001e03e3327
E. Michael Gertz <gertzem@nih.gov>
2021-07-20 11:03:11 -0400

    Add routines to simpilify table features

---
+++ simplify_expression.R
@@ -0,0 +1,42 @@
+suppressPackageStartupMessages({
+  library(org.Hs.eg.db)
+  library(dplyr)
+  library(readr)
+})
+
+args <- commandArgs(trailingOnly = TRUE)
+
+results <- vector("list", length(args))
+for (k in seq_along(args)) {
+  results[[k]] <- read_tsv(args[[k]], col_types = cols()) %>%
+    mutate(
+      direction = ifelse(
+        p_value > 0.05, NA, ifelse(p_greater > .1, "negative", "positive")
+      ),
+      symbol = sapply(
+        mapIds(org.Hs.eg.db,
+          keys = sub("[.].*$", "", genera),
+          column = "SYMBOL", keytype = "ENSEMBL", multiVals = "list"
+        ),
+        paste,
+        collapse = ","
+      )
+    ) %>%
+    select(
+      cancer,
+      versus,
+      symbol,
+      ensembl = genera,
+      median_rank,
+      models_present = seen,
+      direction
+    ) %>%
+    mutate(symbol = ifelse(symbol=='NA', NA, symbol))
+}
+
+features <- bind_rows(results)
+
+features %>%
+  arrange(cancer, versus, median_rank, desc(models_present)) %>%
+  format_tsv() %>%
+  cat()
---
+++ simplify_microbial.R
@@ -0,0 +1,36 @@
+suppressPackageStartupMessages({
+  library(tidyverse)
+})
+
+args <- commandArgs(trailingOnly = TRUE)
+
+results <- vector("list", length(args))
+for (k in seq_along(args)) {
+  results[[k]] <- read_tsv(args[[k]], col_types=cols()) %>%
+    mutate(
+      direction = ifelse(
+        p_value > 0.05, NA, ifelse(p_greater > .1, "negative", "positive")
+      ),
+      univariate_direction = ifelse(
+        univariate_direction == -1, "negative", "positive"
+      ),
+      univariate_fdr = sprintf("%.3g", univariate_fdr)
+    ) %>%
+    select(
+      cancer, 
+      versus,
+      genus = genera,
+      median_rank,
+      models_present = seen,
+      direction,
+      univariate_direction,
+      univariate_fdr
+    )
+}
+
+features <- bind_rows(results)
+
+features %>%
+  arrange(cancer, versus, median_rank, desc(models_present)) %>%
+  format_tsv() %>%
+  cat()
