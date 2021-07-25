750c6f175bf562ab5c72ad19bc076c4566cf465e
Leandro Hermida <hermidal@cs.umd.edu>
2021-07-25 11:39:47 -0400

    Add signif plot filter option; clip stdev bars

--- generate_bar_plots_2.py
+++ generate_bar_plots_2.py
@@ -10,6 +10,7 @@ warnings.filterwarnings('ignore', category=FutureWarning,
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from pandas.api.types import is_object_dtype, is_string_dtype
 import rpy2.rinterface_lib.embedded as r_embedded
 
 r_embedded.set_initoptions(
@@ -30,13 +31,19 @@ if sys.platform.startswith('linux'):
     os.environ['XDG_SESSION_TYPE'] = 'x11'
 
 parser = ArgumentParser()
-parser.add_argument('--results-dir', type=str, default='results/models',
+parser.add_argument('--results-dir', type=str, default='results',
                     help='results dir')
 parser.add_argument('--out-dir', type=str, default='figures', help='out dir')
+parser.add_argument('--filter', type=str, choices=['signif', 'all'],
+                    default='signif',
+                    help='response model filter')
 parser.add_argument('--file-format', type=str, nargs='+',
                     choices=['png', 'pdf', 'svg', 'tif'], default=['png'],
                     help='save file format')
 args = parser.parse_args()
+
+model_results_dir = '{}/models'.format(args.results_dir)
+analysis_results_dir = '{}/analysis'.format(args.results_dir)
 
 os.makedirs(args.out_dir, mode=0o755, exist_ok=True)
 
@@ -51,11 +58,12 @@ colors = ['crimson', 'ocean blue', 'greyish teal', 'steel grey']
 colors = sns.xkcd_palette(colors)
 
 title_fontsize = 16
-axis_fontsize = 12
+x_axis_fontsize = 5 if args.filter == 'all' else 8
+y_axis_fontsize = 12
 legend_fontsize = 8
 fig_let_fontsize = 48
 fig_height = 4
-fig_width = 10
+fig_width = 10 if args.filter == 'all' else 6
 fig_dpi = 300
 bar_width = 0.8
 
@@ -64,11 +72,21 @@ plt.rcParams['font.family'] = ['Nimbus Sans']
 
 r_base = importr('base')
 
+signif_hits = pd.read_csv(
+    '{}/goodness_hits.txt'.format(analysis_results_dir), sep='\t')
+signif_hits = signif_hits.apply(
+    lambda x: x.str.lower() if is_object_dtype(x) or is_string_dtype(x) else x)
+signif_hits = signif_hits.loc[signif_hits['analysis'] == 'resp']
+signif_hits = signif_hits.sort_values(
+    by=['cancer', 'versus', 'features', 'how'])
+signif_hits = signif_hits.loc[signif_hits.duplicated(
+    subset=['cancer', 'versus', 'features'], keep=False)]
+
 all_scores_dfs = {}
 model_codes_regex = '|'.join(model_codes)
 split_results_regex = re.compile(
     '^(.+?_(?:{}))_split_results\\.pkl$'.format(model_codes_regex))
-for dirpath, dirnames, filenames in sorted(os.walk(args.results_dir)):
+for dirpath, dirnames, filenames in sorted(os.walk(model_results_dir)):
     for filename in filenames:
         if m := re.search(split_results_regex, filename):
             model_name = m.group(1)
@@ -78,6 +96,13 @@ for dirpath, dirnames, filenames in sorted(os.walk(args.results_dir)):
                 model_code = '_'.join(rest[1:])
             else:
                 model_code = '_'.join(rest)
+
+            if (args.filter == 'signif' and not (
+                    (signif_hits['cancer'] == cancer)
+                    & (signif_hits['versus'] == target)
+                    & (signif_hits['features'] == data_type)).any()):
+                continue
+
             split_results_file = '{}/{}'.format(dirpath, filename)
             print('Loading', split_results_file)
             split_results = load(split_results_file)
@@ -119,8 +144,10 @@ for data_type in data_types:
                          [x + bar_width for x in x_pos[-1]])
             mean_scores = scores_df.mean(axis=0)
             std_scores = scores_df.std(axis=0)
-            ax.bar(x_pos[-1], mean_scores, yerr=std_scores, align='center',
-                   color=colors[model_idx], ecolor=colors[-1],
+            upper_error = (mean_scores + std_scores).clip(upper=1) - mean_scores
+            lower_error = mean_scores - (mean_scores - std_scores).clip(lower=0)
+            ax.bar(x_pos[-1], mean_scores, yerr=[lower_error, upper_error],
+                   align='center', color=colors[model_idx], ecolor=colors[-1],
                    error_kw=dict(lw=0.75), label=model_code.upper(),
                    width=bar_width, zorder=3)
         ax.axhline(y=0.6, color=colors[-1], linestyle='--', lw=1, zorder=1)
@@ -130,15 +157,16 @@ for data_type in data_types:
         x_labels = [' '.join(n) for n in zip(
             model_name_parts['cancer'].str.upper(),
             model_name_parts['target'].str.title())]
-        ax.set_ylabel(metric_label[metric], fontsize=axis_fontsize, labelpad=5)
+        ax.set_ylabel(metric_label[metric], fontsize=y_axis_fontsize,
+                      labelpad=5)
         x_ticks = [x + bar_width for x in
                    np.arange(0, scores_df.columns.size * 3, step=3)]
-        plt.xticks(x_ticks, x_labels, fontsize=5, rotation=60, weight='bold')
+        plt.xticks(x_ticks, x_labels, fontsize=x_axis_fontsize, rotation=60)
         ax.set_yticks(np.arange(0.0, 1.1, 0.2))
         ax.get_yaxis().set_major_formatter(ticker.FixedFormatter(
             ['0', '0.2', '0.4', '0.6', '0.8', '1']))
         ax.autoscale(axis='x', enable=None, tight=True)
-        ax.set_ylim([0, 1.15])
+        ax.set_ylim([0, 1.3])
         ax.tick_params(axis='x', direction='in', length=0, pad=1)
         ax.margins(0.01)
         ax.grid(False)
@@ -146,7 +174,7 @@ for data_type in data_types:
                            borderpad=0.1, handletextpad=0.25,
                            fontsize=legend_fontsize)
         legend.set_title(data_type_label, prop={'weight': 'bold',
-                                                'size': axis_fontsize})
+                                                'size': y_axis_fontsize})
         legend._legend_box.align = 'right'
         renderer = fig.canvas.get_renderer()
         shift = max([text.get_window_extent(renderer).width
