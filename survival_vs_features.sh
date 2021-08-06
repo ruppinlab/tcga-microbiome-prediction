b7493174fb877d7e236028b03925bbab717bf93c
E. Michael Gertz <gertzem@nih.gov>
2021-08-06 10:09:40 -0400

    Remove the {survival,response}_vs_features directories

--- survival_vs_features.sh
+++
@@ -1,5 +0,0 @@
-Rscript survival_vs_features.R ACC OS > survival_vs_features/ACC_OS.tsv
-Rscript survival_vs_features.R ACC PFI > survival_vs_features/ACC_PFI.tsv
-Rscript survival_vs_features.R CESC OS > survival_vs_features/CESC_OS.tsv
-Rscript survival_vs_features.R LGG PFI > survival_vs_features/LGG_PFI.tsv
-Rscript survival_vs_features.R SKCM OS > survival_vs_features/SKCM_OS.tsv
