a369334dbe4164cd334fe904683f7f1a8f05ba42
Leandro Hermida <hermidal@cs.umd.edu>
2022-04-01 19:44:08 -0400

    Violin figure adjustments; extrafont lib for R pdf export

--- generate_violin_plots.R
+++ generate_violin_plots.R
@@ -1,6 +1,7 @@
 suppressPackageStartupMessages({
     library(argparser)
     library(dplyr)
+    library(extrafont)
     library(gdtools)
     library(ggplot2)
     library(ggsignif)
@@ -29,22 +30,28 @@ argp <- add_argument(
 )
 args <- parse_args(argp)
 
-title_fontsize <- 14
-axis_fontsize <- 14
+stopifnot(
+    args$filter %in% c("goodness_hits", "potential_hits", "compared_runs")
+)
+
+title_fontsize <- 18
+axis_fontsize <- 16
 point_fontsize <- 2
 fig_dim <- 4
 fig_dpi <- 300
 line_color <- "grey"
+
 font_family <- ifelse(
-    font_family_exists("Arial"), "Arial", ifelse(
+    font_family_exists("Helvetica"), "Helvetica", ifelse(
         font_family_exists("DejaVu Sans"), "DejaVu Sans", "sans"
     )
 )
-font_family <- "sans"
-
-stopifnot(
-    args$filter %in% c("goodness_hits", "potential_hits", "compared_runs")
-)
+if (args$file_format == "pdf") {
+    if (font_family != "sans" && !(font_family %in% fonts())) suppressMessages({
+        font_import(pattern=str_replace_all(font_family, " ", ""), prompt=FALSE)
+    })
+    loadfonts(quiet=TRUE)
+}
 
 model_results_dir <- paste(args$results_dir, "models", sep="/")
 analysis_results_dir <- paste(args$results_dir, "analysis", sep="/")
@@ -114,12 +121,17 @@ for (row_idx in seq_len(nrow(signif_hits))) {
     clinical_model_name <- str_replace(
         model_name, paste0(model_code, "$"), clinical_model_code
     )
+    #
     # wihin groups paired violin plot
-    data_type_label <- ifelse(
+    #
+    dtype_label <- ifelse(
         data_type == "kraken", "Microbiome",
         ifelse(data_type == "htseq", "Expression", "Combo")
     )
-    model_label <- paste(data_type_label, "+", "Clinical")
+    abbr_dtype_label <- ifelse(
+        data_type == "kraken", "Micro",
+        ifelse(data_type == "htseq", "Expr", "Combo")
+    )
     fig_num <- ifelse(
         data_type == "kraken", "1", ifelse(target == "os", "Ex1", "Ex2")
     )
@@ -135,7 +147,7 @@ for (row_idx in seq_len(nrow(signif_hits))) {
     data <- data.frame(
         Model=c(
             rep("Clinical", length(clinical_model_scores)),
-            rep(model_label, length(model_scores))
+            rep(paste0(dtype_label, "+", "Clinical"), length(model_scores))
         ),
         Score=c(clinical_model_scores, model_scores)
     )
@@ -154,12 +166,12 @@ for (row_idx in seq_len(nrow(signif_hits))) {
     if (analysis == "surv") {
         title <- paste(title, str_to_upper(target))
     } else {
-        title <- paste(title, str_to_title(target))
+        title <- paste(title, target)
         title <- paste0(title, " (", str_to_upper(model_code), ")")
     }
     p_adj_md <- ifelse(
         !is.na(p_adj), paste0(
-            " *p*<sub>adj</sub> = ", ifelse(
+            "p<sub>adj</sub> = ", ifelse(
                 p_greater <= 0.05, sprintf("%.2e", p_adj), paste0(
                     "<span style='color:red'>", sprintf("%.2e", p_adj),
                     "</span>"
@@ -167,14 +179,45 @@ for (row_idx in seq_len(nrow(signif_hits))) {
             )
         ), ""
     )
-    title <- paste0("**", title, "**", p_adj_md)
+    if (analysis == "surv") {
+        title <- paste(title, p_adj_md)
+        subtitle <- NULL
+        plot_title <- element_markdown(
+            size=title_fontsize, family=font_family, face="plain",
+            margin=margin(5, 0, 1, 0), padding=margin(0, 0, 0, 0)
+        )
+        plot_subtitle <- element_blank()
+    } else {
+        subtitle <- p_adj_md
+        plot_title <- element_text(
+           color="black", face="plain", size=title_fontsize,
+           margin=margin(5, 0, 1, 0)
+        )
+        plot_subtitle <- element_markdown(
+           color="black", face="plain", size=title_fontsize,
+           margin=margin(2, 0, 1, 0), padding=margin(0, 0, 0, 0)
+        )
+    }
+    if (analysis == "surv" && !(cancer %in% c(
+        "chol", "dlbc", "pcpg", "prad", "tgct", "thym", "uvm"
+    ))) {
+        break_start <- 0.2
+        ylim_min <- 0.19
+        ylim_max <- 1.2
+        labels <- c("0.2", "0.4", "0.6", "0.8", "1")
+    } else {
+        break_start <- 0
+        ylim_min <- -0.01
+        ylim_max <- 1.3
+        labels <- c("0", "0.2", "0.4", "0.6", "0.8", "1")
+    }
     p <- ggwithinstats(
         data=data, x="Model", y="Score", xlab="Model", ylab=y_label,
         type="np", centrality.plotting=TRUE, centrality.type="p",
         centrality.label.args=list(label.padding=0.15, size=point_fontsize),
         centrality.point.args=list(color="darkred", size=point_fontsize),
-        point.path.args=list(alpha=0.8, color=line_color),
-        p.adjust.method="BH", results.subtitle=FALSE, title=title,
+        point.path.args=list(alpha=0.8, color=line_color), p.adjust.method="BH",
+        results.subtitle=FALSE, title=title, subtitle=subtitle,
         pairwise.comparisons=TRUE, pairwise.display="all"
     ) +
     theme(
@@ -194,35 +237,19 @@ for (row_idx in seq_len(nrow(signif_hits))) {
         panel.border=element_blank(),
         panel.grid.major.x=element_blank(),
         panel.grid.minor.y=element_blank(),
-        plot.margin=unit(c(0, 2, 0, 2), "pt"),
-        plot.subtitle=element_blank(),
-        plot.title=element_markdown(
-           size=title_fontsize, family=font_family, face="plain",
-           margin=margin(6, 0, 1, 0), padding=margin(0, 0, 0, 0)
-        ),
+        plot.margin=unit(c(0, 10, 0, 2), "pt"),
+        plot.title=plot_title,
+        plot.subtitle=plot_subtitle,
         text=element_text(size=axis_fontsize, family=font_family)
-    )
-    if (
-        analysis == "surv" && !(cancer %in% c(
-            "chol", "dlbc", "pcpg", "prad", "tgct", "thym", "uvm"
-        ))
-    ) {
-        break_start <- 0.2
-        lim_min <- 0.18
-        labels <- c("0.2", "0.4", "0.6", "0.8", "1")
-    } else {
-        break_start <- 0
-        lim_min <- -0.01
-        labels <- c("0", "0.2", "0.4", "0.6", "0.8", "1")
-    }
-    p <- p + scale_y_continuous(
+    ) + scale_y_continuous(
         breaks=seq(break_start, 1, 0.2), expand=c(0, 0),
-        limits=c(lim_min, 1.01), labels=labels
+        limits=c(ylim_min, 1.01), labels=labels
     )
-    suppressMessages(p <- p + scale_color_manual(values=colors))
-    suppressMessages(
-        p <- p + scale_x_discrete(labels=c("Clinical", model_label))
-    )
+    suppressMessages({
+        p <- p + scale_color_manual(values=colors) + scale_x_discrete(
+            labels=c("Clinical", paste0(abbr_dtype_label, "+", "Clinical"))
+        )
+    })
     p$layers <- p$layers[c(4, 1:3, 5:6)]
     p$layers[[2]]$aes_params$size <- 2
     file <- paste(args$out_dir, paste0(
@@ -232,20 +259,25 @@ for (row_idx in seq_len(nrow(signif_hits))) {
         file=file, plot=p, device=args$file_format, width=fig_dim,
         height=fig_dim, units="in", dpi=fig_dpi
     )
-    tfile <- paste(args$out_dir, paste0(
-        str_c(c(model_name, "violin"), collapse="_"),
-        ".tsv"), sep="/")
-    # Include the p_adj score because it is read, not computed.
+    if (args$file_format == "pdf") embed_fonts(file)
+    # include the p_adj score because it is read, not computed
     data_tsv <- tibble(
         cancer = cancer, target = target,
-        data_type = tolower(data_type_label),
+        data_type = tolower(dtype_label),
         model_code = model_code,
         index = seq_along(clinical_model_scores),
         clinical_model_scores = clinical_model_scores,
         model_scores = model_scores,
         p_adj = ifelse(p_greater <= 0.05, p_adj, 1))
+    tfile <- paste(
+        args$out_dir,
+        paste0(str_c(c(model_name, "violin"), collapse="_"), ".tsv"),
+        sep="/"
+    )
     write_tsv(data_tsv, tfile)
+    #
     # between groups combo comparison violin plot
+    #
     if (data_type != "combo") next
     model_name_parts <- str_split(model_name, "_")[[1]]
     kraken_model_name <- str_c(c(
@@ -283,13 +315,13 @@ for (row_idx in seq_len(nrow(signif_hits))) {
     ) %>%
     arrange(.data = ., group1)
     pw_cmps$label <- str_replace(
-        pw_cmps$label, fixed("[FDR-corrected]"), "[adj]"
+        pw_cmps$label, fixed("italic(p)[FDR-corrected]"), "p[adj]"
     )
     title <- str_to_upper(cancer)
     if (analysis == "surv") {
         title <- paste(title, str_to_upper(target))
     } else {
-        title <- paste(title, str_to_title(target))
+        title <- paste(title, target)
         title <- paste0(title, " (", str_to_upper(model_code), ")")
     }
     colors <- c("#448ee4", "#c04e01", "#94568c")
@@ -305,14 +337,15 @@ for (row_idx in seq_len(nrow(signif_hits))) {
     ) +
     geom_signif(
         comparisons=pw_cmps$groups[c(1, 2)], annotations=pw_cmps$label[c(1, 2)],
-        na.rm=TRUE, parse=TRUE, test=NULL, tip_length=0.02, vjust=0.07,
-        y_position=c(1.005, 1.080)
+        na.rm=TRUE, parse=TRUE, test=NULL, step_increase=0.07, textsize=5,
+        tip_length=0.02, vjust=0.07, y_position=c(1.005, 1.080)
     ) +
     theme(
         aspect.ratio=1,
         axis.title.x=element_blank(),
         axis.text.x=element_text(
-            color="black", face="plain", size=axis_fontsize
+            color="black", face="plain", size=axis_fontsize, angle=-15,
+            vjust=0.1
         ),
         axis.title.y=element_text(
             color="black", face="plain", size=axis_fontsize
@@ -325,22 +358,21 @@ for (row_idx in seq_len(nrow(signif_hits))) {
         panel.border=element_blank(),
         panel.grid.major.x=element_blank(),
         panel.grid.minor.y=element_blank(),
-        plot.margin=unit(c(0, 2, 0, 2), "pt"),
-        plot.title=element_markdown(
-           size=title_fontsize, family=font_family,
-           margin=margin(6, 0, 1, 0), padding=margin(0, 0, 0, 0)
-        ),
+        plot.margin=unit(c(0, 10, 0, 2), "pt"),
+        plot.title=element_text(
+           color="black", face="plain", size=title_fontsize,
+           margin=margin(5, 0, 1, 0)
+       ),
         plot.subtitle=element_blank(),
         text=element_text(size=axis_fontsize, family=font_family)
     ) + scale_y_continuous(
-        breaks=seq(break_start, 1.2, 0.2), expand=c(0, 0),
-        limits=c(lim_min, 1.2), labels=c(labels, "")
+        breaks=seq(break_start, 1, 0.2), expand=c(0, 0),
+        limits=c(ylim_min, ylim_max), labels=labels
     )
     suppressMessages({
-        p <- p + scale_color_manual(values=colors)
-    })
-    suppressMessages({
-        p <- p + scale_x_discrete(labels=c("Microbiome", "Expression", "Combo"))
+        p <- p + scale_color_manual(values=colors) + scale_x_discrete(
+            labels=c("Microbiome", "Expression", "Combo")
+        )
     })
     p$layers <- p$layers[c(1:5, 7)]
     file <- paste(args$out_dir, paste0(
@@ -351,15 +383,18 @@ for (row_idx in seq_len(nrow(signif_hits))) {
         file=file, plot=p, device=args$file_format, width=fig_dim,
         height=fig_dim, units="in", dpi=fig_dpi
     )
+    if (args$file_format == "pdf") embed_fonts(file)
     data_tsv <- tibble(
        cancer = cancer, target = target,
-       data_type = tolower(data_type_label),
+       data_type = tolower(dtype_label),
        index = seq_along(kraken_model_scores),
        kraken_model_scores = kraken_model_scores,
        htseq_model_scores = htseq_model_scores,
        combo_model_scores = combo_model_scores)
-    tfile <- paste(args$out_dir, paste0(
-        str_c(c(model_name, "violin", "comp"), collapse="_"),
-        ".tsv"), sep="/")
+    tfile <- paste(
+        args$out_dir,
+        paste0(str_c(c(model_name, "violin", "comp"), collapse="_"), ".tsv"),
+        sep="/"
+    )
     write_tsv(data_tsv, tfile)
 }
