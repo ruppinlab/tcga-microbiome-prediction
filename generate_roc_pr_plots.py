04c543fe00cee3a70f2c492a3fa4f5baed227332
Leandro Hermida <hermidal@cs.umd.edu>
2022-04-02 08:08:06 -0400

    Fix syntax error

--- generate_roc_pr_plots.py
+++ generate_roc_pr_plots.py
@@ -42,7 +42,7 @@ fig_dpi = 300
 plt.rcParams['figure.max_open_warning'] = 0
 plt.rcParams['font.family'] = 'sans-serif'
 plt.rcParams['font.sans-serif'] = ['Helvetica', 'Nimbus Sans', 'Arial',
-                                   'DejaVu Sans', 'sans-serif']]
+                                   'DejaVu Sans', 'sans-serif']
 
 pipe_step_type_regex = re.compile(
     r'^({})\d+$'.format('|'.join(['slr', 'trf', 'clf'])))
