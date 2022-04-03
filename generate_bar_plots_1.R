c622e4810d52a9d02b047e3229eba8a115e13422
Leandro Hermida <hermidal@cs.umd.edu>
2022-04-03 16:19:47 -0400

    Fix to prevent Rplots.pdf being generated

--- generate_bar_plots_1.R
+++ generate_bar_plots_1.R
@@ -9,6 +9,8 @@ suppressPackageStartupMessages({
   library(rstatix)
 })
 
+if (!interactive()) pdf(NULL)
+
 color_palette <- c(
   "#999999", "#E69F00", "#56B4E9", "#009E73",
   "#F0E442", "#0072B2", "#D55E00", "#CC79A7"
