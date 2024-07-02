131c694bb7461fac93b6685c3c07d1227e07d709
Leandro Hermida <softdev@leandrohermida.com>
2024-07-02 11:44:14 -0400

    Add Ge et al. bioRxiv 2024 data preprocessing code and config

---
+++ create_salzberg_esets.R
@@ -0,0 +1,373 @@
+options(warn = 1)
+suppressPackageStartupMessages({
+    library(argparser)
+    library(Biobase)
+    library(data.table)
+    library(GenomicDataCommons)
+    library(stringr)
+})
+
+stopifnot(GenomicDataCommons::status()$status == "OK")
+
+argp <- arg_parser("Create ExpressionSets")
+argp <- add_argument(
+    argp, "--data-dir",
+    default = "data", help = "Data directory"
+)
+argp <- add_argument(argp, "--cancers", nargs = Inf, help = "TCGA cancer codes")
+argp <- add_argument(argp, "--surv-types", nargs = Inf, help = "Survival types")
+argp <- add_argument(argp, "--drug-names", nargs = Inf, help = "Drug names")
+argp <- add_argument(
+    argp, "--gdc-workflow-types",
+    default = c("HTSeq - Counts"),
+    help = "GDC workflow types"
+)
+argp <- add_argument(
+    argp, "--save-data-matrix",
+    flag = TRUE,
+    help = "Save data matrix rds files"
+)
+args <- parse_args(argp)
+
+sample_types <- c(
+    "primary_tumor", "primary_blood_derived_cancer_-_peripheral_blood"
+)
+
+cat("Loading salzberg_kraken_meta.rds\n")
+kraken_meta <- readRDS(
+    paste(args$data_dir, "salzberg_kraken_meta.rds", sep = "/")
+)
+cat("Loading salzberg_kraken_data.rds\n")
+kraken_data <- readRDS(
+    paste(args$data_dir, "salzberg_kraken_data.rds", sep = "/")
+)
+cat("Loading response_pdata.rds\n")
+response_pdata <- readRDS(paste(args$data_dir, "response_pdata.rds", sep = "/"))
+cat("Loading survival_pdata.rds\n")
+survival_pdata <- readRDS(paste(args$data_dir, "survival_pdata.rds", sep = "/"))
+
+# generate datasets
+min_uniq_cases <- 10
+min_uniq_cases_per_class <- 3
+min_uniq_case_exceptions <- c()
+
+create_surv_eset <- function(
+    adata, pdata, fdata, cancer, surv_type, data_type, msg_prefix,
+    print_sample_msgs = TRUE) {
+    eset <- ExpressionSet(
+        assayData = adata, phenoData = AnnotatedDataFrame(pdata)
+    )
+    if (!is.null(fdata)) featureData(eset) <- AnnotatedDataFrame(fdata)
+    status_col <- toupper(surv_type)
+    time_col <- paste(status_col, "time", sep = "_")
+    eset$Status <- eset[[status_col]]
+    eset$Survival_in_days <- eset[[time_col]]
+    eset <- eset[
+        , !is.na(eset[[status_col]]) & !is.na(eset[[time_col]]) &
+            eset[[time_col]] > 0
+    ]
+    pData(eset) <- gdata::drop.levels(pData(eset))
+    if (anyDuplicated(eset$case_submitter_id)) {
+        eset$Group <- match(
+            eset$case_submitter_id, unique(eset$case_submitter_id)
+        )
+        if ("experimental_strategy" %in% colnames(pData(eset))) {
+            case_strategy <- as.data.frame(table(data.frame(
+                case_submitter_id = factor(eset$case_submitter_id),
+                experimental_strategy = factor(eset$experimental_strategy)
+            )))
+            case_strategy <- case_strategy[case_strategy$Freq != 0, ]
+            max_freq <- aggregate(Freq ~ case_submitter_id, case_strategy, max)
+            colnames(max_freq)[colnames(max_freq) == "Freq"] <- "MaxFreq"
+            case_strategy <-
+                merge(case_strategy, max_freq, by = "case_submitter_id")
+            case_strategy$GroupWeight <-
+                case_strategy$MaxFreq / case_strategy$Freq
+            eset$GroupWeight <- case_strategy$GroupWeight[match(
+                paste(eset$case_submitter_id, eset$experimental_strategy),
+                paste(
+                    case_strategy$case_submitter_id,
+                    case_strategy$experimental_strategy
+                )
+            )]
+        } else if ("GroupWeight" %in% colnames(pData(eset))) {
+            eset$GroupWeight <- NULL
+        }
+    } else {
+        if ("Group" %in% colnames(pData(eset))) eset$Group <- NULL
+        if ("GroupWeight" %in% colnames(pData(eset))) eset$GroupWeight <- NULL
+    }
+    cancer_target <- gsub("^tcga ", "", paste(
+        tolower(gsub("(-|\\s)+", " ", c(cancer, surv_type))),
+        collapse = " "
+    ))
+    if (
+        length(unique(eset$case_submitter_id)) >= min_uniq_cases ||
+            cancer_target %in% min_uniq_case_exceptions
+    ) {
+        if (print_sample_msgs) {
+            cat(
+                msg_prefix, toupper(surv_type),
+                length(unique(eset$case_submitter_id)), "cases",
+                ncol(eset), "samples", "\n"
+            )
+        }
+        eset_filename_parts <- c(cancer, "surv", surv_type, data_type, "eset")
+        eset_filename <- paste0(paste(
+            tolower(gsub("(-|\\s)+", "_", eset_filename_parts)),
+            collapse = "_"
+        ), ".rds")
+        cat(msg_prefix, "Creating", eset_filename, "\n")
+        saveRDS(eset, paste(args$data_dir, eset_filename, sep = "/"))
+    } else {
+        cat(msg_prefix, paste("Skipping", toupper(surv_type)), "\n")
+    }
+}
+
+create_drug_eset <- function(
+    adata, pdata, fdata, cancer, resp_type, drug_name, data_type, msg_prefix,
+    print_sample_msgs = TRUE) {
+    eset <- ExpressionSet(
+        assayData = adata, phenoData = AnnotatedDataFrame(pdata)
+    )
+    if (!is.null(fdata)) featureData(eset) <- AnnotatedDataFrame(fdata)
+    eset$Class <- ifelse(
+        eset$response %in% c(
+            "Complete Response", "Partial Response"
+        ), 1,
+        ifelse(eset$response %in% c(
+            "Clinical Progressive Disease", "Stable Disease"
+        ), 0, NA)
+    )
+    eset$Class <- as.factor(eset$Class)
+    eset <- eset[, !is.na(eset$Class)]
+    pData(eset) <- gdata::drop.levels(pData(eset))
+    if (anyDuplicated(eset$case_submitter_id)) {
+        eset$Group <- match(
+            eset$case_submitter_id, unique(eset$case_submitter_id)
+        )
+        num_uniq_cases_per_class <- aggregate(
+            case_submitter_id ~ Class, pData(eset),
+            function(x) length(unique(x))
+        )$case_submitter_id
+    } else {
+        if ("Group" %in% colnames(pData(eset))) eset$Group <- NULL
+        num_uniq_cases_per_class <- table(eset$Class)
+    }
+    cancer_target <- gsub("^tcga ", "", paste(
+        tolower(gsub("(-|\\s)+", " ", c(cancer, drug_name))),
+        collapse = " "
+    ))
+    if (
+        (
+            length(unique(eset$case_submitter_id)) >= min_uniq_cases ||
+                cancer_target %in% min_uniq_case_exceptions
+        ) &&
+            length(unique(eset$Class)) > 1 &&
+            min(num_uniq_cases_per_class) >= min_uniq_cases_per_class
+    ) {
+        if (print_sample_msgs) {
+            cat(
+                msg_prefix, drug_name, length(unique(eset$case_submitter_id)),
+                "cases", ncol(eset), "samples\n"
+            )
+        }
+        eset_filename_parts <-
+            c(cancer, resp_type, drug_name, data_type, "eset")
+        eset_filename <- paste0(paste(
+            tolower(gsub("(-|\\s)+", "_", eset_filename_parts)),
+            collapse = "_"
+        ), ".rds")
+        cat(msg_prefix, "Creating", eset_filename, "\n")
+        saveRDS(eset, paste(args$data_dir, eset_filename, sep = "/"))
+    }
+}
+
+get_gdc_data <- function(project_id, workflow_types, msg_prefix) {
+    file_query <-
+        files() %>%
+        filter(
+            cases.project.project_id %in% project_id &
+                analysis.workflow_type %in% workflow_types
+        ) %>%
+        GenomicDataCommons::select(c(
+            "file_name",
+            "analysis.workflow_type",
+            "cases.project.project_id",
+            "cases.case_id",
+            "cases.submitter_id",
+            "cases.samples.sample_id",
+            "cases.samples.submitter_id",
+            "cases.samples.sample_type",
+            "cases.samples.is_ffpe",
+            "cases.samples.portions.is_ffpe",
+            "cases.samples.portions.analytes.aliquots.aliquot_id",
+            "cases.samples.portions.analytes.aliquots.submitter_id"
+        ))
+    file_results <- results_all(file_query)
+    file_meta <- data.frame(
+        file_uuid = file_results$file_id,
+        file_name = file_results$file_name,
+        workflow_type = file_results$analysis$workflow_type,
+        project_id = vapply(
+            sapply(file_results$cases, `[[`, "project"), `[`, "project_id"
+        ),
+        case_uuid = sapply(file_results$cases, `[[`, "case_id"),
+        case_submitter_id = sapply(file_results$cases, `[[`, "submitter_id"),
+        sample_uuid = sapply(
+            sapply(file_results$cases, `[[`, "samples"), `[[`, "sample_id"
+        ),
+        sample_submitter_id = sapply(
+            sapply(file_results$cases, `[[`, "samples"), `[[`, "submitter_id"
+        ),
+        sample_type = sapply(
+            sapply(file_results$cases, `[[`, "samples"), `[[`, "sample_type"
+        ),
+        sample_is_ffpe = sapply(
+            sapply(file_results$cases, `[[`, "samples"), `[[`, "is_ffpe"
+        ),
+        portion_is_ffpe = sapply(
+            sapply(
+                sapply(file_results$cases, `[[`, "samples"), `[[`, "portions"
+            ), `[[`, "is_ffpe"
+        ),
+        aliquot_uuid = sapply(
+            sapply(
+                sapply(
+                    sapply(
+                        sapply(
+                            file_results$cases, `[[`, "samples"
+                        ), `[[`, "portions"
+                    ), `[[`, "analytes"
+                ), `[[`, "aliquots"
+            ), `[[`, "aliquot_id"
+        ),
+        aliquot_submitter_id = sapply(
+            sapply(
+                sapply(
+                    sapply(
+                        sapply(
+                            file_results$cases, `[[`, "samples"
+                        ), `[[`, "portions"
+                    ), `[[`, "analytes"
+                ), `[[`, "aliquots"
+            ), `[[`, "submitter_id"
+        ),
+        row.names = file_results$file_id,
+        stringsAsFactors = FALSE
+    )
+    file_meta <- file_meta[order(
+        file_meta$project_id, file_meta$case_submitter_id,
+        file_meta$sample_submitter_id, file_meta$aliquot_submitter_id
+    ), ]
+    cat(msg_prefix, "Downloading", length(file_results$id), "GDC files\n")
+    files <- gdcdata(ids(file_query), progress = FALSE, use_cached = TRUE)
+    return(list(meta = file_meta, files = files))
+}
+
+# GENCODE annots
+gtf_annots_filename <- "gencode_v22_ensg_v98_annots.tsv"
+gtf_annots_file <- paste(args$data_dir, gtf_annots_filename, sep = "/")
+cat("Loading", gtf_annots_file, "\n")
+rna_annots <- read.delim(
+    gtf_annots_file,
+    row.names = 1, stringsAsFactors = FALSE
+)
+rna_annots$Symbol[grepl(".", rna_annots$Symbol, fixed = TRUE)] <- ""
+combo_annots <- rbind(data.frame(
+    Symbol = rep("", ncol(kraken_data)), row.names = colnames(kraken_data)
+), rna_annots)
+
+gdc_meta_pdata_cols <- c(
+    "project_id", "case_uuid", "case_submitter_id", "sample_uuid",
+    "sample_submitter_id", "sample_is_ffpe", "portion_is_ffpe", "aliquot_uuid",
+    "aliquot_submitter_id", "file_uuid"
+)
+if (any(is.na(args$cancers))) {
+    cancers <- sort(union(response_pdata$cancer, survival_pdata$cancer))
+} else {
+    cancers <- sort(paste("TCGA", toupper(args$cancers), sep = "-"))
+}
+cancer_msg_pad <- max(str_length(cancers))
+if (!any(is.na(args$surv_types))) {
+    surv_types <- sort(tolower(args$surv_types))
+} else if (any(is.na(args$drug_names))) {
+    surv_types <- c("os", "pfi")
+} else {
+    surv_types <- c()
+}
+resp_types <- c("resp")
+type_msg_pad <- max(str_length(c("kraken", "combo", args$gdc_workflow_types)))
+
+uniq_rna_case_uuids <- c()
+uniq_rna_sample_uuids <- c()
+
+cat("Generating datasets\n")
+for (cancer in cancers) {
+    # kraken
+    data_type <- "kraken"
+    msg_prefix <- paste(
+        "[",
+        str_pad(cancer, cancer_msg_pad, side = "right"),
+        str_pad("Kraken", type_msg_pad, side = "right"),
+        "]"
+    )
+    # survival
+    kraken_surv_meta <- merge(
+        survival_pdata[survival_pdata$cancer == cancer, , drop = FALSE],
+        kraken_meta,
+        by = "case_submitter_id"
+    )
+    if (nrow(kraken_surv_meta) == 0) {
+        cat(msg_prefix, paste("No data"), "\n")
+        next
+    }
+    row.names(kraken_surv_meta) <- kraken_surv_meta$hopkins_id
+    kraken_surv_data <-
+        t(kraken_data)[, row.names(kraken_surv_meta), drop = FALSE]
+    kraken_surv_meta <-
+        kraken_surv_meta[order(row.names(kraken_surv_meta)), , drop = FALSE]
+    kraken_surv_data <-
+        kraken_surv_data[, order(colnames(kraken_surv_data)), drop = FALSE]
+    for (surv_type in surv_types) {
+        create_surv_eset(
+            kraken_surv_data, kraken_surv_meta, NULL, cancer, surv_type,
+            data_type, msg_prefix
+        )
+    }
+    # response
+    if (!any(is.na(args$drug_names))) {
+        drug_names <- sort(str_to_title(args$drug_names))
+    } else if (any(is.na(args$surv_types))) {
+        drug_names <- sort(
+            unique(response_pdata$drug.name[response_pdata$cancer == cancer])
+        )
+    } else {
+        drug_names <- c()
+    }
+    for (drug_name in drug_names) {
+        kraken_drug_meta <- merge(
+            response_pdata[
+                response_pdata$cancer == cancer &
+                    response_pdata$drug.name == drug_name, ,
+                drop = FALSE
+            ],
+            kraken_meta,
+            by = "case_submitter_id"
+        )
+        if (nrow(kraken_drug_meta) == 0) next
+        row.names(kraken_drug_meta) <- kraken_drug_meta$hopkins_id
+        kraken_drug_data <-
+            t(kraken_data)[, row.names(kraken_drug_meta), drop = FALSE]
+        kraken_drug_meta <-
+            kraken_drug_meta[order(row.names(kraken_drug_meta)), , drop = FALSE]
+        kraken_drug_data <-
+            kraken_drug_data[, order(colnames(kraken_drug_data)), drop = FALSE]
+        for (idx in seq_along(resp_types)) {
+            create_drug_eset(
+                kraken_drug_data, kraken_drug_meta, NULL, cancer,
+                resp_types[idx], drug_name, data_type, msg_prefix, idx == 1
+            )
+        }
+    }
+}
---
+++ process_salzberg_data_gdc_meta.R
@@ -0,0 +1,210 @@
+options(warn = 1)
+suppressPackageStartupMessages({
+    library(argparser)
+    library(httr)
+    library(GenomicDataCommons)
+    library(readxl)
+    library(stringr)
+})
+
+stopifnot(GenomicDataCommons::status()$status == "OK")
+
+argp <- arg_parser("Process Salzberg kraken data and metadata")
+argp <- add_argument(
+    argp, "--data-dir",
+    default = "data", help = "Data directory"
+)
+argp <- add_argument(argp, "--debug", flag = TRUE, help = "Debug mode")
+args <- parse_args(argp)
+
+# Salzberg Kraken data
+kraken_msg_pad <- 15
+sample_msg_pad <- 5
+
+cat("Processing Salzberg Kraken data\n")
+kraken_base_url <-
+    "https://github.com/yge15/TCGA_Microbial_Content/raw/main/"
+kraken_data_filename <-
+    "TableS1_Kraken-TCGA-WGS-5734-Samples-11349-Species.Microbial2023_noEuk.RawCounts.xlsx"
+kraken_data_url <- paste0(kraken_base_url, kraken_data_filename)
+kraken_data_file <- paste(args$data_dir, kraken_data_filename, sep = "/")
+if (file.exists(kraken_data_file)) {
+    cat("Using existing", kraken_data_file, "\n")
+} else {
+    cat("Downloading", kraken_data_filename, "\n")
+    download.file(kraken_data_url, kraken_data_file)
+}
+kraken_data <- as.data.frame(
+    read_excel(kraken_data_file, progress = FALSE),
+    stringsAsFactors = FALSE
+)
+row.names(kraken_data) <- kraken_data$HopkinsID
+kraken_data$HopkinsID <- NULL
+kraken_data <- as.matrix(kraken_data)
+storage.mode(kraken_data) <- "integer"
+kraken_data <- kraken_data[!(rownames(kraken_data) == "Sum"), ]
+
+kraken_meta_filename <- "TableS12_Metadata-TCGA-WGS-5734-Samples.xlsx"
+kraken_meta_url <- paste0(kraken_base_url, kraken_meta_filename)
+kraken_meta_file <- paste(args$data_dir, kraken_meta_filename, sep = "/")
+if (file.exists(kraken_meta_file)) {
+    cat("Using existing", kraken_meta_file, "\n")
+} else {
+    cat("Downloading", kraken_meta_filename, "\n")
+    download.file(kraken_meta_url, kraken_meta_file)
+}
+kraken_meta <- as.data.frame(
+    read_excel(kraken_meta_file, progress = FALSE),
+    stringsAsFactors = FALSE
+)
+colnames(kraken_meta) <- c(
+    "hopkins_id", "knight_id", "staussman_id", "file_uuid",
+    "project_id", "case_uuid", "sample_uuid", "sample_type",
+    "aliquot_uuid", "aliquot_submitter_id", "rd_len", "tot_rds",
+    "grch38_unmapped", "grch38+chm13_unmapped"
+)
+row.names(kraken_meta) <- kraken_meta$hopkins_id
+kraken_meta$case_uuid <- tolower(kraken_meta$case_uuid)
+kraken_meta$sample_uuid <- tolower(kraken_meta$sample_uuid)
+kraken_meta$aliquot_uuid <- tolower(kraken_meta$aliquot_uuid)
+kraken_meta$file_uuid <- tolower(kraken_meta$file_uuid)
+
+cat(
+    "[", str_pad("Kraken", kraken_msg_pad, side = "right"), "]",
+    nrow(kraken_meta), "samples", length(unique(kraken_meta$case_uuid)),
+    "unique cases\n"
+)
+
+# sample type filter
+sample_types <- c(
+    "primary_tumor", "primary_blood_derived_cancer_-_peripheral_blood"
+)
+kraken_meta <- kraken_meta[kraken_meta$sample_type %in% sample_types, ]
+
+kraken_data <- kraken_data[row.names(kraken_meta), ]
+cat(
+    "[", str_pad("Kraken Filtered", kraken_msg_pad, side = "right"), "]",
+    nrow(kraken_meta), "samples", length(unique(kraken_meta$case_uuid)),
+    "unique cases\n"
+)
+
+# GDC case metadata
+gdc_case_results_filename <- "gdc_case_results.rds"
+gdc_case_results_file <- paste(
+    args$data_dir, gdc_case_results_filename,
+    sep = "/"
+)
+if (file.exists(gdc_case_results_file)) {
+    cat("Using existing", gdc_case_results_file, "\n")
+    gdc_case_results <- readRDS(gdc_case_results_file)
+} else {
+    cat("Downloading GDC case metadata\n")
+    gdc_case_query <-
+        cases() %>%
+        filter(case_id %in% kraken_meta$case_uuid) %>%
+        GenomicDataCommons::select(c(
+            "project.project_id",
+            "submitter_id",
+            "diagnoses.age_at_diagnosis",
+            "demographic.gender",
+            "diagnoses.ajcc_pathologic_stage"
+        ))
+    gdc_case_results <- results_all(gdc_case_query)
+    cat(paste("Writing", gdc_case_results_filename), "\n")
+    saveRDS(gdc_case_results, gdc_case_results_file)
+}
+for (i in seq_along(gdc_case_results$diagnoses)) {
+    # set NULL diagnoses inner list elements to NA
+    if (is.null(gdc_case_results$diagnoses[[i]])) {
+        gdc_case_results$diagnoses[[i]] <- data.frame(
+            age_at_diagnosis = NA, ajcc_pathologic_stage = NA
+        )
+    } else if (
+        # set non-existent diagnoses.ajcc_pathologic_stage to "Not Reported"
+        is.null(gdc_case_results$diagnoses[[i]]$ajcc_pathologic_stage)
+    ) {
+        gdc_case_results$diagnoses[[i]]$ajcc_pathologic_stage <- "Not Reported"
+    }
+}
+gdc_case_meta <- data.frame(
+    project_id = gdc_case_results$project$project_id,
+    case_uuid = gdc_case_results$case_id,
+    case_submitter_id = gdc_case_results$submitter_id,
+    gender = gdc_case_results$demographic$gender,
+    age_at_diagnosis = sapply(
+        gdc_case_results$diagnoses, `[[`, "age_at_diagnosis"
+    ),
+    tumor_stage = sapply(
+        gdc_case_results$diagnoses, `[[`, "ajcc_pathologic_stage"
+    ),
+    row.names = NULL,
+    stringsAsFactors = FALSE
+)
+gdc_case_meta$tumor_stage <- tolower(gdc_case_meta$tumor_stage)
+gdc_case_meta$tumor_stage <- gsub(
+    "^stage\\s+", "", gdc_case_meta$tumor_stage
+)
+gdc_case_meta$tumor_stage <- gsub(
+    "i/ii\\s+nos", "i or ii", gdc_case_meta$tumor_stage
+)
+gdc_case_meta$tumor_stage <- gsub(
+    "(a|b|c|s)$", "", gdc_case_meta$tumor_stage
+)
+gdc_case_meta$tumor_stage <- gsub(
+    "^(not reported|x)$", NA, gdc_case_meta$tumor_stage
+)
+kraken_meta$case_submitter_id <- gdc_case_meta$case_submitter_id[
+    match(kraken_meta$case_uuid, gdc_case_meta$case_uuid)
+]
+gdc_case_meta$case_uuid <- NULL
+
+gdc_aliquot_results_filename <- "gdc_aliquot_results.rds"
+gdc_aliquot_results_file <- paste(
+    args$data_dir, gdc_aliquot_results_filename,
+    sep = "/"
+)
+if (file.exists(gdc_aliquot_results_file)) {
+    cat("Using existing", gdc_aliquot_results_file, "\n")
+    gdc_aliquot_results <- readRDS(gdc_aliquot_results_file)
+} else {
+    cat("Downloading GDC aliquot metadata\n")
+    gdc_aliquot_query <-
+        cases() %>%
+        filter(
+            samples.portions.analytes.aliquots.aliquot_id
+            %in% kraken_meta$aliquot_uuid
+        ) %>%
+        GenomicDataCommons::select(c(
+            "aliquot_ids",
+            "submitter_aliquot_ids"
+        ))
+    gdc_aliquot_results <- results_all(gdc_aliquot_query)
+    cat(paste("Writing", gdc_aliquot_results_filename), "\n")
+    saveRDS(gdc_aliquot_results, gdc_aliquot_results_file)
+}
+gdc_aliquot_meta <- data.frame(
+    aliquot_uuid = unlist(
+        gdc_aliquot_results$aliquot_ids,
+        use.names = FALSE
+    ),
+    aliquot_submitter_id = unlist(
+        gdc_aliquot_results$submitter_aliquot_ids,
+        use.names = FALSE
+    ),
+    row.names = NULL,
+    stringsAsFactors = FALSE
+)
+kraken_meta$aliquot_submitter_id <- gdc_aliquot_meta$aliquot_submitter_id[
+    match(kraken_meta$aliquot_uuid, gdc_aliquot_meta$aliquot_uuid)
+]
+
+cat("Writing salzberg_kraken_meta.rds\n")
+saveRDS(
+    kraken_meta, paste(args$data_dir, "salzberg_kraken_meta.rds", sep = "/")
+)
+cat("Writing salzberg_kraken_data.rds\n")
+saveRDS(
+    kraken_data, paste(args$data_dir, "salzberg_kraken_data.rds", sep = "/")
+)
+cat("Writing gdc_case_meta.rds\n")
+saveRDS(gdc_case_meta, paste(args$data_dir, "gdc_case_meta.rds", sep = "/"))
