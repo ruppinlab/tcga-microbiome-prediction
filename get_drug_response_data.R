8376583c47fcf41facbc504bd4905c130959aaf3
Leandro Hermida <softdev@leandrohermida.com>
2024-09-14 06:21:25 -0400

    Refactor better code; remove drug response output gets generated

--- get_drug_response_data.R
+++ get_drug_response_data.R
@@ -1,5 +1,6 @@
 options(warn = 1)
 suppressPackageStartupMessages({
+    library(dplyr)
     library(GenomicDataCommons)
     library(stringr)
     library(TCGAbiolinks)
@@ -7,11 +8,12 @@ suppressPackageStartupMessages({
 
 stopifnot(GenomicDataCommons::status()$status == "OK")
 
-project_ids <- sort(
+project_ids <-
     projects() %>%
     GenomicDataCommons::filter(program.name == "TCGA") %>%
-    ids()
-)
+    ids() %>%
+    sort()
+
 gdc_query <- GDCquery(
     project = project_ids,
     data.category = "Clinical",
@@ -23,53 +25,59 @@ suppressMessages(clinical_data <- GDCprepare(gdc_query))
 
 drug_names <- read.delim("data/tcga_drug_names.tsv", sep = "\t")
 drug_names$tcga_name <- toupper(drug_names$tcga_name)
-stopifnot(length(unique(drug_names$tcga_name)) == nrow(drug_names))
+stopifnot(!any(duplicated(drug_names$tcga_name)))
 
-drug_response_data <- data.frame()
+drug_response_dfs <- list()
 for (project_id in project_ids) {
     cat(paste("Processsing", project_id), "\n")
-    cancer <- str_split_fixed(project_id, "-", n = 2)[2]
-    drug_df_name <- paste("clinical_drug", tolower(cancer), sep = "_")
     # DLBC and LAML have no drug response clinical data
     if (project_id %in% c("TCGA-DLBC", "TCGA-LAML")) next
+    cancer <- str_split_fixed(project_id, "-", n = 2)[2]
+    drug_df_name <- paste("clinical_drug", tolower(cancer), sep = "_")
     stopifnot(drug_df_name %in% names(clinical_data))
     gdc_drug_data <- as.data.frame(clinical_data[[drug_df_name]])
     stopifnot(
         gdc_drug_data[1, 1] == "bcr_patient_uuid" &&
-        gdc_drug_data[2, 1] == "CDE_ID:"
+            gdc_drug_data[2, 1] == "CDE_ID:"
     )
-    gdc_drug_data <- gdc_drug_data[3:nrow(gdc_drug_data), ]
+    gdc_drug_data <- gdc_drug_data[3:nrow(gdc_drug_data), , drop = FALSE]
     gdc_drug_data$pharmaceutical_therapy_drug_name <- toupper(
         gdc_drug_data$pharmaceutical_therapy_drug_name
     )
-
     missing_drug_names <- setdiff(
         gdc_drug_data$pharmaceutical_therapy_drug_name, drug_names$tcga_name
     )
     if (length(missing_drug_names) > 0) {
         cat(paste(project_id, "missing drug names:", missing_drug_names), "\n")
     }
-
-    drug_response_data <- rbind(
-        drug_response_data,
-        data.frame(
-            cancer = rep(cancer, nrow(gdc_drug_data)),
-            case_submitter_id = gdc_drug_data$bcr_patient_barcode,
-            drug_name = drug_names$standard_name[match(
-                gdc_drug_data$pharmaceutical_therapy_drug_name,
-                drug_names$tcga_name
-            )],
-            start_time = gdc_drug_data$pharmaceutical_tx_started_days_to,
-            end_time = gdc_drug_data$pharmaceutical_tx_ended_days_to,
-            response = gdc_drug_data$treatment_best_response
+    drug_response_dfs[[project_id]] <-
+        inner_join(
+            gdc_drug_data, drug_names,
+            by = join_by(pharmaceutical_therapy_drug_name == tcga_name)
+        ) %>%
+        mutate(cancer = cancer, .before = everything()) %>%
+        dplyr::select(
+            cancer,
+            bcr_patient_barcode,
+            standard_name,
+            pharmaceutical_tx_started_days_to,
+            pharmaceutical_tx_ended_days_to,
+            treatment_best_response
+        ) %>%
+        rename(
+            case_submitter_id = bcr_patient_barcode,
+            drug_name = standard_name,
+            start_time = pharmaceutical_tx_started_days_to,
+            end_time = pharmaceutical_tx_ended_days_to,
+            response = treatment_best_response
         )
-    )
 }
 
+drug_response_data <- bind_rows(drug_response_dfs)
 drug_response_data <- drug_response_data[!(
     drug_response_data$response %in%
-    c("[Unknown]", "[Not Applicable]", "[Not Available]", "[Discrepancy]"
-)), ]
+        c("[Unknown]", "[Not Applicable]", "[Not Available]", "[Discrepancy]")
+), ]
 write.table(
     drug_response_data, "data/tcga_drug_response.tsv",
     sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE
