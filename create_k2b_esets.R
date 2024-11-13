59e51b3ee0b8ceb8bb220a76350e881403f64243
Leandro Hermida <softdev@leandrohermida.com>
2024-11-13 09:47:30 -0500

    Add filtering

--- create_k2b_esets.R
+++ create_k2b_esets.R
@@ -69,8 +69,10 @@ create_surv_eset <- function(
         , !is.na(eset[[status_col]]) & !is.na(eset[[time_col]]) &
             eset[[time_col]] > 0
     ]
-    eset <- eset[rowSums(exprs(eset)) != 0, ]
-    eset <- eset[, colSums(exprs(eset)) != 0]
+    exprs(eset)[exprs(eset) < 10] <- 0
+    eset <- eset[
+        rowSums(exprs(eset)) != 0, colSums(exprs(eset)) != 0, drop = FALSE
+    ]
     pData(eset) <- gdata::drop.levels(pData(eset))
     if (anyDuplicated(eset$case_submitter_id)) {
         eset$Group <- match(
@@ -146,8 +148,10 @@ create_drug_eset <- function(
     )
     eset$Class <- as.factor(eset$Class)
     eset <- eset[, !is.na(eset$Class)]
-    eset <- eset[rowSums(exprs(eset)) != 0, ]
-    eset <- eset[, colSums(exprs(eset)) != 0]
+    exprs(eset)[exprs(eset) < 10] <- 0
+    eset <- eset[
+        rowSums(exprs(eset)) != 0, colSums(exprs(eset)) != 0, drop = FALSE
+    ]
     pData(eset) <- gdata::drop.levels(pData(eset))
     if (anyDuplicated(eset$case_submitter_id)) {
         eset$Group <- match(
